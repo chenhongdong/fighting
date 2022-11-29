f5e3f81b8ed253a033caf5a34cac901b94216ddc
chenhongdong <513948670@qq.com>
2022-11-29 17:33:34 +0800

    29日算法题

---
+++ 每日写5道算法/二叉树/221129-路径总和II.js
@@ -0,0 +1,27 @@
+/* 
+2022/11/29
+给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+叶子节点 是指没有子节点的节点。
+
+示例1：
+https://assets.leetcode.com/uploads/2021/01/18/pathsumii1.jpg
+输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+输出：[[5,4,11,2],[5,8,4,5]]
+
+示例2：
+https://assets.leetcode.com/uploads/2021/01/18/pathsum2.jpg
+输入：root = [1,2,3], targetSum = 5
+输出：[]
+
+
+https://leetcode.cn/problems/path-sum-ii/description/
+*/
+
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number[][]}
+ */
+const pathSum = (root, target) => {
+
+}
